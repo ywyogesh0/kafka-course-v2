33b8810f0617eacad83d765559389d921d7f296c
Yogesh Walia <ywalia@sapient.com>
2018-10-23 09:04:43 +0530

    Second Commit

---
+++ kafka-basics/src/main/java/kafka/practice/constants/Constant.java
@@ -0,0 +1,12 @@
+package kafka.practice.constants;
+
+        import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Constant {
+
+    public static final String TOPIC_VALUE = "topic-23-oct-18";
+
+    public static final String BOOTSTRAP_SERVERS_VALUE = "localhost:9092";
+    public static final String KEY_SERIALIZER_CLASS_VALUE = StringSerializer.class.getName();
+    public static final String VALUE_SERIALIZER_CLASS_VALUE = StringSerializer.class.getName();
+}
---
+++ kafka-basics/src/main/java/kafka/practice/producers/ProducerDemo.java
@@ -0,0 +1,34 @@
+package kafka.practice.producers;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import static kafka.practice.constants.Constant.*;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+
+    public static void main(String[] args) {
+
+        // create properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_VALUE);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KEY_SERIALIZER_CLASS_VALUE);
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, VALUE_SERIALIZER_CLASS_VALUE);
+
+        // create kafka producer
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>(TOPIC_VALUE, "Today is 23rd Oct, 18");
+
+        // send records - asynchronus call
+        producer.send(producerRecord);
+
+    }
+}
